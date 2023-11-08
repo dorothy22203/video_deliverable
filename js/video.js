764c928bd3fb85e7959e9914ccbb9c0325cab0aa
dorothy22203 <145305931+dorothy22203@users.noreply.github.com>
2023-11-08 02:20:27 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,62 +1,62 @@
 var video;
 
 window.addEventListener("load", function() {
-    
-    video = document.querySelector("video");
+    console.log("Window opened successfully");
+    video = document.querySelector("#player1");
     video.autoplay = false;
     video.loop = false;
+    updateVolumeDisplay();
 
-    function updateVolumeInfo() {
-        console.log("Current Volume: " + (video.volume * 100).toFixed(1) + "%");
+    function updateVolumeDisplay() {
+        document.getElementById("volume").innerHTML = video.volume * 100 + "%";
+    }
+
+    function updateSpeedDisplay() {
+        console.log("New speed is " + video.playbackRate);
     }
 
     document.querySelector("#play").addEventListener("click", function() {
         console.log("Play Video");
-        if (video.paused) {
-            video.play();
-            updateVolumeInfo();
-        }
+        video.play();
+        updateVolumeDisplay();
     });
 
     document.querySelector("#pause").addEventListener("click", function() {
         console.log("Pause Video");
-        if (!video.paused) {
-            video.pause();
-        }
+        video.pause();
     });
 
-    document.querySelector("#slowDown").addEventListener("click", function() {
-        console.log("Slow Down Video");
-        video.playbackRate -= 0.1;
-        console.log("New Speed: " + video.playbackRate.toFixed(1));
+    document.querySelector("#slower").addEventListener("click", function() {
+        video.playbackRate *= 0.9;
+        updateSpeedDisplay();
     });
 
-    document.querySelector("#speedUp").addEventListener("click", function() {
-        console.log("Speed Up Video");
-        video.playbackRate += 0.1;
-        console.log("New Speed: " + video.playbackRate.toFixed(1));
+    document.querySelector("#faster").addEventListener("click", function() {
+        video.playbackRate /= 0.9;
+        updateSpeedDisplay();
     });
 
-    document.querySelector("#skipAhead").addEventListener("click", function() {
-        console.log("Skip Ahead");
-        video.currentTime += 10;
-        if (video.currentTime >= video.duration) {
-            video.currentTime = 0;
-        }
-        console.log("Current Time: " + video.currentTime.toFixed(1));
+    document.querySelector("#skip").addEventListener("click", function() {
+        video.currentTime = Math.min(video.currentTime + 10, video.duration);
+        console.log("Current location is " + video.currentTime);
     });
 
     document.querySelector("#mute").addEventListener("click", function() {
-        console.log("Toggle Mute");
         video.muted = !video.muted;
-        document.querySelector("#mute").textContent = video.muted ? "Unmute" : "Mute";
-        updateVolumeInfo();
+        this.innerHTML = video.muted ? "Unmute" : "Mute";
     });
 
-    document.querySelector("#volumeSlider").addEventListener("input", function() {
-        console.log("Volume Changed");
-        video.volume = document.querySelector("#volumeSlider").value / 100;
-        updateVolumeInfo();
+    document.querySelector("#slider").addEventListener("input", function() {
+        video.volume = this.value / 100;
+        updateVolumeDisplay();
+        console.log("Volume is: " + video.volume * 100 + "%");
     });
 
+    document.querySelector("#vintage").addEventListener("click", function() {
+        video.classList.add("oldSchool");
+    });
+
+    document.querySelector("#orig").addEventListener("click", function() {
+        video.classList.remove("oldSchool");
+    });
 });
